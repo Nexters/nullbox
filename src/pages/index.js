c89208683b26e9f941023f06c8232e309d7bb9d8
hyunsik <hyunsik.dev@gmail.com>
2018-08-22 21:28:10 +0900

    merge

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 export { default as Home } from './Home/Home';
-export { default as Game } from './Game/Game';
 export { default as Setting } from './Setting/Setting';
 export { default as Korean } from './Korean/Korean';
 export { default as Score } from './Score/Score';
